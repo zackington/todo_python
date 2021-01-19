0619ab52823a55e6295118902e1f5003ffc34b11
zackington <zagatol96@gmail.com>
2021-01-19 22:14:07 +0600

    static_files

--- todo/urls.py
+++ todo/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage, name= 'home'),
     path('test', test,  name = 'test'),
-    path('test2/', third),
+    path('test3/', third),
 ]
