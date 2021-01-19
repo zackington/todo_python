24fd3680d0a8b66245e76647b550f1f7933de7c7
zackington <zagatol96@gmail.com>
2021-01-19 17:21:52 +0600

    fate

--- main/views.py
+++ main/views.py
@@ -6,5 +6,5 @@ def homepage(requests):
 def test(requests):
     return render(requests, "test.html") 
 
-def second(requests):
-    return HttpResponse("test 2 page")       
+def third(requests):
+    return HttpResponse("This is page test3")       
